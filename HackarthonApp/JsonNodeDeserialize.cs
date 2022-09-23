bfbff7d36a3b3eff9f23cae3acafbcf0ee3aa89a
Ignok <bok.ignacy@gmail.com>
2022-09-23 23:29:08 +0200

    Cleaned up code

--- HackarthonApp/JsonNodeDeserialize.cs
+++ HackarthonApp/JsonNodeDeserialize.cs
@@ -8,39 +8,32 @@ namespace HackarthonApp
     {
         public static void ShowJsonInfo(string jsonString)
         {
-            // Create a JsonNode DOM from a JSON string.
+            // Create a JsonNode DOM from a JSON string
             JsonNode artworkNode = JsonNode.Parse(jsonString)!;
 
             // Write JSON from a JsonNode
             var options = new JsonSerializerOptions { WriteIndented = true };
-            //Console.WriteLine(artworkNode!.ToJsonString(options));
-
-            //JsonNode idNode = artworkNode!["Id"]!;
-            Console.WriteLine($"Type={artworkNode.GetType()}");
 
             JsonNode data = artworkNode!["data"]!;
-            Console.WriteLine($"Type={data.GetType()}");
-            //Console.WriteLine($"JSON={data.ToJsonString()}");
 
             string id = (artworkNode["data"]!["id"]!).ToString();
-            Console.WriteLine($"data.id={id}");
+            Console.WriteLine($" -id={id}");
 
             string type = (artworkNode["data"]!["type"]!).ToString();
-            Console.WriteLine($"data.type={type}");
+            Console.WriteLine($" -type={type}");
 
             string att = (artworkNode["data"]!["attributes"]!).ToString();
-            //Console.WriteLine($"data.attributes={att}");
 
             string date = (artworkNode["data"]!["attributes"]!["Date"]!).ToString();
-            Console.WriteLine($"data.attributes={date}");
+            Console.WriteLine($" -date={date}");
+
+            string category = (artworkNode["data"]!["attributes"]!["Category"]!["Name"]!).ToString();
+            Console.WriteLine($" -category={category}");
 
             JsonNode titles = (artworkNode["data"]!["attributes"]!["Title"]!);
             JsonNode firstTitle = titles[0]!;
             string title = firstTitle["Title"]!.ToString();
-            Console.WriteLine($"data.attributes.title.title={title}");
-
-            //int idInt = (int)artworkNode!["Id"]!;
-            //Console.WriteLine($"Value={idInt}");
+            Console.WriteLine($" -title={title}");
         }
     }
 }
--- HackarthonApp/Program.cs
+++ HackarthonApp/Program.cs
@@ -5,24 +5,17 @@ namespace HackarthonApp
     {
         static HttpClient client = new HttpClient();
 
-        static async void GetArtworkAsync(string path)
+        static async Task<string?> GetArtworkAsync(string path)
         {
             HttpResponseMessage response = await client.GetAsync(path);
+            Console.WriteLine("Response code: " + response.StatusCode);
+            string data;
             if (response.IsSuccessStatusCode)
             {
-                string data = await response.Content.ReadAsStringAsync();
-                //JavaScriptSerializer JSserializer = new JavaScriptSerializer();
-
-                //artwork = JSserializer.Deserialize<Artwork>(data);
-                //Console.WriteLine("received object: " + artwork);
-                //Console.WriteLine("received object: " + data);
-                JsonNodeDeserialize.ShowJsonInfo(data);
+                data = await response.Content.ReadAsStringAsync();
+                return data;
             }
-            Console.WriteLine("response code: " + response.StatusCode);
-            if (response.IsSuccessStatusCode)
-            {
-                Console.WriteLine("Success status!");
-            }
+            return null;
         }
 
         static void Main(string[] args)
@@ -31,34 +24,38 @@ namespace HackarthonApp
             Console.WriteLine("Hello World!");
         }
 
+        private static string url = "https://apihackaton.zacheta.art.pl/api/v1/";
         private static string urlParameters = "6b62e11399cdc969ce8c7778a1d9ba9194f791a3b33dddd3f91dc8d2c91bb19cf7a1b63bcc8f23029c5751a03557263fb92489754e3a63650bfb0f8c4e72d12fa3716011732cfdb6c30462759d288bfdf6c6b7740c0e7f6d7bed492b710198317aceab6950426bf5a61f74b7aaafa2efd063bd473f6fd3a1bc79f35dc009e151";
-
+        private static string id = "7";
+        private static string date = "2004";
         static async Task RunAsync()
         {
-            client.BaseAddress = new Uri("https://apihackaton.zacheta.art.pl/api/v1/");
+            client.BaseAddress = new Uri(url);
             client.DefaultRequestHeaders.Accept.Clear();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + urlParameters);
 
-            /*                new MediaTypeWithQualityHeaderValue("application/json"));
-            */
-
             try
             {
-                // Get the artwork
-                GetArtworkAsync("https://apihackaton.zacheta.art.pl/api/v1/artworks/1");
-                //Console.WriteLine("received artwork's id: " + artwork.Id);
-                //ShowArtwork(artwork);
+                // Get the artwork with given id
+                Console.WriteLine("Searching info about artwork id: " + id);
+                string? data = await GetArtworkAsync(url + "artworks/" + id);
+                if (data != null)
+                {
+                    JsonNodeDeserialize.ShowJsonInfo(data);
+                    Console.WriteLine();
+                }
+                else
+                {
+                    Console.WriteLine("Error: Artwork with given id doesn't exist");
+                }
+                //mając dostep do konkretnych atrybutów można już filtrować itd.
+
+
             }
             catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
-
-            /*HttpResponseMessage response = client.GetAsync(urlParameters).Result;
-            if (response.IsSuccessStatusCode)
-            {
-                
-            }*/
 
             Console.ReadLine();
         }
